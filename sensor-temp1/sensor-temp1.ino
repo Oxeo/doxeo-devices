d7f64ae589ce8e107f9c401dd0ccddb6acf91453
Snyfir <Snyfir@users.noreply.github.com>
2017-11-08 20:27:13 +0100

    Fix temp power on to reduce power

--- sensor-temp1/sensor-temp1.ino
+++ sensor-temp1/sensor-temp1.ino
@@ -10,6 +10,8 @@
 #include <DoxeoConfig.h>
 
 #define PIN_TEMPERATURE 4
+#define PIN_ALIM_TEMPERATURE 5
+#define TEMPERATURE_PRECISION 11 //Max 12 bit, min 9 bit
 
 // Temperature sensor
 OneWire oneWire(PIN_TEMPERATURE);
@@ -29,8 +31,9 @@ DallasTemperature sensors(&oneWire);
 
 int cpt = 0;
 byte data[32];
+byte tempAddress[8];
 
-void setup() {
+void setup() { 
   // Configure NRF communication
   Mirf.cePin = 9; // Broche CE sur D9
   Mirf.csnPin = 10; // Broche CSN sur D10
@@ -49,15 +52,32 @@ void setup() {
   while (Mirf.isSending());
   Mirf.powerDown();
 
+  // configure temperature sensor
+  pinMode(PIN_ALIM_TEMPERATURE, OUTPUT);
+  digitalWrite(PIN_ALIM_TEMPERATURE, HIGH);
+  delay(50); // Allow 50ms for the sensor to be ready
+  sensors.begin(); 
+  sensors.setWaitForConversion(false);   
+  int numSensors=sensors.getDeviceCount();
+  oneWire.search(tempAddress);
+  sensors.setResolution(tempAddress, TEMPERATURE_PRECISION);
+
   cpt = 0;
 }
 
 void loop() {
   if (cpt % 75 == 0) {
     // take temperature
-    sensors.requestTemperatures();
+    pinMode(PIN_ALIM_TEMPERATURE, OUTPUT); // set power pin for DS18B20 to output  
+    digitalWrite(PIN_ALIM_TEMPERATURE, HIGH);
+    LowPower.powerDown(SLEEP_30MS, ADC_OFF, BOD_OFF);
+    sensors.setResolution(tempAddress, TEMPERATURE_PRECISION); 
+    sensors.requestTemperatures(); // Send the command to get temperatures  
+    LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF); // 9bit requres 95ms, 10bit 187ms, 11bit 375ms and 12bit resolution takes 750ms
     String message = "temperature1;";
-    message += sensors.getTempCByIndex(0);
+    message += sensors.getTempC(tempAddress);
+    digitalWrite(PIN_ALIM_TEMPERATURE, LOW); // turn DS18B20 sensor off
+    pinMode(PIN_ALIM_TEMPERATURE, INPUT); // set power pin for DS18B20 to input before sleeping, saves power 
 
     // send
     message.getBytes(data, 32);
@@ -72,4 +92,5 @@ void loop() {
   LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   cpt++;
 }
+
 
