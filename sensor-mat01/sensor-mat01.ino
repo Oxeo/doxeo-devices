90d11d49a4b0371a644dfe8fd169ed6eb80eec64
Snyfir <Snyfir@users.noreply.github.com>
2017-12-03 18:05:10 +0100

    Fix mat sensor not emitting

--- sensor-mat01/sensor-mat01.ino
+++ sensor-mat01/sensor-mat01.ino
@@ -23,6 +23,9 @@
 byte data[32];
 
 void setup() {
+  pinMode(PIN_WAKEUP, INPUT);
+  pinMode(PIN_ALIM, OUTPUT);
+  
   // Configure NRF communication
   Mirf.cePin = 9; // Broche CE sur D9
   Mirf.csnPin = 10; // Broche CSN sur D10
@@ -34,9 +37,6 @@ void setup() {
   Mirf.configRegister(RF_SETUP, 0x26); // sortie 0dBm @ 250Kbs to improve distance
   Mirf.setTADDR((byte *) DOXEO_ADDR_MOTHER); // Adresse de transmission
   Mirf.setRADDR((byte *) DOXEO_ADDR_MAT01); // Adresse de réception
-
-  pinMode(PIN_WAKEUP, INPUT);
-  pinMode(PIN_ALIM, OUTPUT);
 
   digitalWrite(PIN_ALIM, HIGH);
 
@@ -50,7 +50,7 @@ void wakeUp()
 
 void loop() {
   // Allow wake up pin to trigger interrupt on change.
-  attachInterrupt(digitalPinToInterrupt(PIN_WAKEUP), wakeUp, HIGH);
+  attachInterrupt(digitalPinToInterrupt(PIN_WAKEUP), wakeUp, CHANGE);
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
   detachInterrupt(digitalPinToInterrupt(PIN_WAKEUP));
   digitalWrite(PIN_ALIM, LOW);
@@ -61,6 +61,7 @@ void loop() {
     digitalWrite(PIN_ALIM, LOW);
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
     digitalWrite(PIN_ALIM, HIGH);
+    LowPower.powerDown(SLEEP_30MS, ADC_OFF, BOD_OFF);
   } while (digitalRead(PIN_WAKEUP) == HIGH);
 
   sendStatus("off");
@@ -70,10 +71,22 @@ void sendStatus(String state) {
   String message = "doormat;" + state;
   message.getBytes(data, 32);
 
+  // Configure NRF communication
+  Mirf.cePin = 9; // Broche CE sur D9
+  Mirf.csnPin = 10; // Broche CSN sur D10
+  Mirf.spi = &MirfHardwareSpi; // On veut utiliser le port SPI hardware
+  Mirf.init(); // Initialise la bibliothèque
+  Mirf.channel = 1; // Choix du canal de communication (128 canaux disponibles, de 0 à 127)
+  Mirf.payload = 32; // Taille d'un data (maximum 32 octets)
+  Mirf.config(); // Sauvegarde la configuration dans le module radio
+  Mirf.configRegister(RF_SETUP, 0x26); // sortie 0dBm @ 250Kbs to improve distance
+  Mirf.setTADDR((byte *) DOXEO_ADDR_MOTHER); // Adresse de transmission
+  Mirf.setRADDR((byte *) DOXEO_ADDR_MAT01); // Adresse de réception
+
   for (int i=0; i<5; ++i) {
     Mirf.send(data);
     while (Mirf.isSending());
-    LowPower.powerDown(SLEEP_60MS, ADC_OFF, BOD_OFF);
+    LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
   }
 
   // power down NRF to save energy
