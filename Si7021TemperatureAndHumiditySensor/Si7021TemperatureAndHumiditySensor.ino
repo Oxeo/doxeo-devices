b2c45188b444b617f2c4edc0ab4c56541b42a64d
Snyfir <Snyfir@users.noreply.github.com>
2018-11-18 16:53:11 +0100

    change si7021

--- Si7021TemperatureAndHumiditySensor/Si7021TemperatureAndHumiditySensor.ino
+++ Si7021TemperatureAndHumiditySensor/Si7021TemperatureAndHumiditySensor.ino
@@ -1,35 +1,3 @@
-/**
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************
- *
- * REVISION HISTORY
- * Version 1.0: Yveaux
- * 
- * DESCRIPTION
- * This sketch provides an example of how to implement a humidity/temperature
- * sensor using a Si7021 sensor.
- *  
- * For more information, please visit:
- * http://www.mysensors.org/build/humiditySi7021
- * 
- */
-
 // Enable debug prints
 //#define MY_DEBUG
 
@@ -78,6 +46,11 @@ void presentation()
 
 void setup()
 {
+  #ifdef MY_DEBUG
+    Serial.print(F("setup in progress!"));
+  #endif
+
+
   while (not sensor.begin())
   {
     Serial.println(F("Sensor not detected!"));
@@ -103,8 +76,8 @@ void loop()
   static MyMessage msgHum( CHILD_ID_HUM,  V_HUM );
   static MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
 
-  send(msgTemp.set(temperature, 2));
-  send(msgHum.set(humidity, 2));
+  send(msgTemp.set(temperature, 1));
+  send(msgHum.set(humidity, 1));
 
 #ifdef REPORT_BATTERY_LEVEL
   const uint8_t batteryPcnt = static_cast<uint8_t>(0.5 + vcc.Read_Perc(VccMin, VccMax));
