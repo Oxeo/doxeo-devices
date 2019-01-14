13172cd980c6d7e8503cb1d3d2d8c4bf0e927291
Snyfir <Snyfir@users.noreply.github.com>
2019-01-14 22:33:01 +0100

    Add battery level

--- DallasTemperature/DallasTemperature.ino
+++ DallasTemperature/DallasTemperature.ino
@@ -1,6 +1,9 @@
 // Enable debug prints to serial monitor
 //#define MY_DEBUG
 
+// Enable REPORT_BATTERY_LEVEL to measure battery level and send changes to gateway
+#define REPORT_BATTERY_LEVEL
+
 #define MY_RADIO_RF24
 #define MY_RF24_PA_LEVEL (RF24_PA_MAX)
 #define MY_TRANSPORT_MAX_TX_FAILURES (3u)
@@ -12,7 +15,7 @@
 
 // 1 = lounge
 // 2 = bedroom
-#define SENSOR_CONFIGURATION 2
+#define SENSOR_CONFIGURATION 1
 
 #if SENSOR_CONFIGURATION == 1
 #define PIN_TEMPERATURE 3
@@ -20,6 +23,15 @@
 #elif SENSOR_CONFIGURATION == 2
 #define PIN_TEMPERATURE 4
 #define PIN_ALIM_TEMPERATURE 5
+#endif
+
+#ifdef REPORT_BATTERY_LEVEL
+#include <Vcc.h>
+static uint8_t oldBatteryPcnt = 200;  // Initialize to 200 to assure first time value will be sent.
+const float VccMin        = 2.8;      // Minimum expected Vcc level, in Volts: Brownout at 2.8V    -> 0%
+const float VccMax        = 2.0*1.6;  // Maximum expected Vcc level, in Volts: 2xAA fresh Alkaline -> 100%
+const float VccCorrection = 1.0;      // Measured Vcc by multimeter divided by reported Vcc
+static Vcc vcc(VccCorrection); 
 #endif
 
 #define TEMPERATURE_PRECISION 11 //Max 12 bit, min 9 bit
@@ -69,6 +81,24 @@ void loop() {
       }
     }
   }
+
+#ifdef REPORT_BATTERY_LEVEL
+  const uint8_t batteryPcnt = static_cast<uint8_t>(0.5 + vcc.Read_Perc(VccMin, VccMax));
+
+#ifdef MY_DEBUG
+  Serial.print(F("Vbat "));
+  Serial.print(vcc.Read_Volts());
+  Serial.print(F("\tPerc "));
+  Serial.println(batteryPcnt);
+#endif
+
+  // Battery readout should only go down. So report only when new value is smaller than previous one.
+  if ( batteryPcnt < oldBatteryPcnt )
+  {
+      sendBatteryLevel(batteryPcnt);
+      oldBatteryPcnt = batteryPcnt;
+  }
+#endif
 }
 
 void initializeDallasSensor() {
