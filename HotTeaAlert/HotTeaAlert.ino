19a3bb5b6f6be6d6fe898094968326d8395ca345
Snyfir <Snyfir@users.noreply.github.com>
2019-01-23 23:10:06 +0100

    HotTeaAlert: Change received msg to switch

--- HotTeaAlert/HotTeaAlert.ino
+++ HotTeaAlert/HotTeaAlert.ino
@@ -2,42 +2,42 @@
 #define MY_RADIO_RF24
 #define MY_RF24_PA_LEVEL (RF24_PA_MAX)
 
-#define REPORT_BATTERY_LEVEL
-
 #include <MySensors.h>
+#include <Vcc.h>
 #include <Wire.h>  // A4 => SDA, A5 => SCL
 
 #define I2C_ADDR 0x5A // Adresse par défaut du capteur IR
-#define TEMP_ID 0
-#define TARGET_ID 1
-#define STATE_ID 2
+#define STATE_ID 0
+#define TEMP_ID 1
 #define TEMP_POWER A0
+#define SWITCH_PIN 2
 #define RED_LED_PIN 3
 #define GREEN_LED_PIN 4
 #define BLUE_LED_PIN 5
 #define EEPROM_TARGET_TEMP 0
+#define EEPROM_MODE 1
 
-MyMessage temperatureMsg(TEMP_ID, V_TEMP);
 MyMessage stateMsg(STATE_ID, V_TEXT);
+MyMessage temperatureMsg(TEMP_ID, V_TEMP);
 
 enum State_enum {SLEEPING, STARTING, HOT, READY, COLD};
+const char *_stateString[] = {"Sleeping", "Starting", "Hot", "Ready", "Cold"};
 
-int targetTemperature = -100;
-int oldTemperature = -100;
-uint8_t state = SLEEPING;
-unsigned long coldCpt = 0;
+int _targetTemperature = -100;
+int _oldTemperature = -100;
+uint8_t _state = SLEEPING;
+unsigned long _coldCpt = 0;
+int _mode = 1;
 
-#ifdef REPORT_BATTERY_LEVEL
-#include <Vcc.h>
-static uint8_t oldBatteryPcnt = 200;  // Initialize to 200 to assure first time value will be sent.
-const float VccMin        = 2.8;      // Minimum expected Vcc level, in Volts: Brownout at 2.8V    -> 0%
-const float VccMax        = 2.0 * 1.6; // Maximum expected Vcc level, in Volts: 2xAA fresh Alkaline -> 100%
-const float VccCorrection = 1.0;      // Measured Vcc by multimeter divided by reported Vcc
-static Vcc vcc(VccCorrection);
-#endif
+static uint8_t _oldBatteryPcnt = 200;  // Initialize to 200 to assure first time value will be sent.
+const float _vccMin        = 2.8;      // Minimum expected Vcc level, in Volts: Brownout at 2.8V    -> 0%
+const float _vccMax        = 2.0 * 1.6; // Maximum expected Vcc level, in Volts: 2xAA fresh Alkaline -> 100%
+const float _vccCorrection = 1.0;      // Measured Vcc by multimeter divided by reported Vcc
+static Vcc _vcc(_vccCorrection);
 
 void before()
 {
+  pinMode(SWITCH_PIN, INPUT);
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(BLUE_LED_PIN, OUTPUT);
@@ -51,7 +51,7 @@ void before()
   digitalWrite(BLUE_LED_PIN, HIGH);
   wait(500);
   digitalWrite(BLUE_LED_PIN, LOW);
-  
+
   pinMode(RED_LED_PIN, INPUT);
   pinMode(GREEN_LED_PIN, INPUT);
   pinMode(BLUE_LED_PIN, INPUT);
@@ -60,154 +60,202 @@ void before()
 void presentation()
 {
   sendSketchInfo("Hot Tea Alert", "1.0");
-  present(TEMP_ID, S_TEMP, "Tea temperature");
-  present(TARGET_ID, S_TEMP, "Target temperature");
   present(STATE_ID, S_INFO, "State status");
+  present(TEMP_ID, S_TEMP, "Tea temperature");
 }
 
 void setup()
 {
-  state = SLEEPING;
-  coldCpt = 0;
-  oldTemperature = -100;
-  targetTemperature = getTargetTemperature();
+  _state = SLEEPING;
+  _coldCpt = 0;
+  _oldTemperature = -100;
+  loadTargetTemperature();
+  loadMode();
 
-  // Initialisation du bus I2C
-  Wire.begin();
+  Wire.begin(); // Initialisation du bus I2C
 
-#ifdef MY_DEBUG
-  Serial.print(F("Target Temperature in EEPROM: "));
-  Serial.println(targetTemperature);
-#endif
-
-  request(TARGET_ID, V_TEMP); // request target temperature
-  wait(4000); // wait 4 seconds for answer
-
-
-#ifdef REPORT_BATTERY_LEVEL
   reportBatteryLevel();
-#endif
 }
 
 void loop()
 {
   int temperature = round(getTemperature());
 
-#ifdef MY_DEBUG
-  Serial.print(F("Temperature: "));
-  Serial.println(temperature);
-#endif
+  #ifdef MY_DEBUG
+    Serial.print(F("Temperature: "));
+    Serial.println(temperature);
+  #endif
 
-  if (state == SLEEPING) {
+  if (_state == SLEEPING) {
     if (temperature < 40) {
-      sleep(30000); // 30 seconds
+      if (sleep(0, FALLING, 30000) == 0) {
+        delay(3000);
+        if (digitalRead(SWITCH_PIN) == LOW) {
+          if (_mode == 1) {
+            changeMode(2);
+          } else {
+            changeMode(1);
+          }
+        }
+      }
     } else {
       pinMode(RED_LED_PIN, OUTPUT);
       pinMode(GREEN_LED_PIN, OUTPUT);
       pinMode(BLUE_LED_PIN, OUTPUT);
-      oldTemperature = -100;
-      state = STARTING;
-      #ifdef MY_DEBUG
-        Serial.print(F("State: STARTING"));
-      #endif
+      _oldTemperature = -100;
+      changeState(STARTING);
     }
   } else {
-      if (oldTemperature != temperature) {
+    if (_oldTemperature != temperature) {
+      _oldTemperature = temperature;
+
+      if (_mode == 2) {
         send(temperatureMsg.set(temperature));
-        oldTemperature = temperature;
-
-        if (temperature > targetTemperature && state != HOT) {
-          digitalWrite(RED_LED_PIN, HIGH);
-          digitalWrite(GREEN_LED_PIN, LOW);
-          digitalWrite(BLUE_LED_PIN, LOW);
-          send(stateMsg.set("HOT"));
-          state = HOT;
-          #ifdef MY_DEBUG
-            Serial.print(F("State: HOT"));
-          #endif
-        } else if (temperature <= targetTemperature
-                   && temperature >= targetTemperature - 10
-                   && state != READY) {
-          digitalWrite(RED_LED_PIN, LOW);
-          digitalWrite(GREEN_LED_PIN, HIGH);
-          digitalWrite(BLUE_LED_PIN, LOW);
-          send(stateMsg.set("READY"));
-          state = READY;
-          #ifdef MY_DEBUG
-            Serial.print(F("State: READY"));
-          #endif
-        } else if (temperature < targetTemperature - 10 && state != COLD) {
-          digitalWrite(RED_LED_PIN, LOW);
-          digitalWrite(GREEN_LED_PIN, LOW);
-          digitalWrite(BLUE_LED_PIN, HIGH);
-          send(stateMsg.set("COLD"));
-          coldCpt = 0;
-          state = COLD;
-          #ifdef MY_DEBUG
-            Serial.print(F("State: COLD"));
-          #endif
-        }
       }
 
-      if (state == COLD) {
-        coldCpt++;
-      }
-
-      if (coldCpt >= 600) {  // sleep after 10 minutes
-        digitalWrite(RED_LED_PIN, LOW);
+      if (temperature > _targetTemperature && _state != HOT) {
+        digitalWrite(RED_LED_PIN, HIGH);
         digitalWrite(GREEN_LED_PIN, LOW);
         digitalWrite(BLUE_LED_PIN, LOW);
-        pinMode(RED_LED_PIN, INPUT);
-        pinMode(GREEN_LED_PIN, INPUT);
-        pinMode(BLUE_LED_PIN, INPUT);
-        send(stateMsg.set("SLEEPING"));
-        state = SLEEPING;
-        #ifdef MY_DEBUG
-            Serial.print(F("State: SLEEPING"));
-        #endif
-      } else {
-        sleep(1000);
+        changeState(HOT);
+      } else if (temperature <= _targetTemperature
+                 && temperature >= _targetTemperature - 10
+                 && _state != READY) {
+        digitalWrite(RED_LED_PIN, LOW);
+        digitalWrite(GREEN_LED_PIN, HIGH);
+        digitalWrite(BLUE_LED_PIN, LOW);
+        changeState(READY);
+      } else if (temperature < _targetTemperature - 10 && _state != COLD) {
+        digitalWrite(RED_LED_PIN, LOW);
+        digitalWrite(GREEN_LED_PIN, LOW);
+        digitalWrite(BLUE_LED_PIN, HIGH);
+        _coldCpt = 0;
+        changeState(COLD);
       }
     }
+
+    if (_state == COLD) {
+      _coldCpt++;
+    }
+
+    if (_coldCpt >= 600) {  // sleep after 10 minutes
+      digitalWrite(RED_LED_PIN, LOW);
+      digitalWrite(GREEN_LED_PIN, LOW);
+      digitalWrite(BLUE_LED_PIN, LOW);
+      pinMode(RED_LED_PIN, INPUT);
+      pinMode(GREEN_LED_PIN, INPUT);
+      pinMode(BLUE_LED_PIN, INPUT);
+      reportBatteryLevel();
+      changeState(SLEEPING);
+    } else {
+      if (sleep(0, FALLING, 1000) == 0) {
+        delay(500);
+        if (digitalRead(SWITCH_PIN) == LOW) {
+          saveTargetTemperature(temperature);
+        }
+      }
+    }
+  }
 }
 
-void receive(const MyMessage &myMsg)
-{
-  if (myMsg.sensor == TARGET_ID && myMsg.type == V_TEMP) {
-    int newTarget = myMsg.getInt();
+void changeState(uint8_t state) {
+  _state = state;
+  
+  #ifdef MY_DEBUG
+      Serial.print(F("State: "));
+      Serial.println(_stateString[state]);
+  #endif
+
+  send(stateMsg.set(_stateString[state]));
+}
+
+void saveTargetTemperature(int temperature) {
+  if (temperature == _targetTemperature) {
+    return;
+  }
+
+  if (temperature < 30 || temperature > 200) {
+    return;
+  }
+
+  saveState(EEPROM_TARGET_TEMP, temperature);
+  _targetTemperature = temperature;
 
 #ifdef MY_DEBUG
-    Serial.print(F("Target Temperature received: "));
-    Serial.println(newTarget);
+  Serial.print(F("New target temperature: "));
+  Serial.println(_targetTemperature);
 #endif
 
-    if (newTarget >= 30 && newTarget <= 200 && targetTemperature != newTarget) {
-      targetTemperature = newTarget;
-      //saveState(EEPROM_TARGET_TEMP, newTarget);
-      
-      pinMode(RED_LED_PIN, OUTPUT);
-      for (char i = 0; i < 10; i++) {
-        if (i % 2 == 0) {
-          digitalWrite(RED_LED_PIN, HIGH);
-        } else {
-          digitalWrite(RED_LED_PIN, LOW);
-        }
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, LOW);
+  digitalWrite(BLUE_LED_PIN, LOW);
 
-        delay(50);
-      }
-      pinMode(RED_LED_PIN, INPUT);
+  for (char i = 0; i < 40; i++) {
+    if (i % 2 == 0) {
+      digitalWrite(RED_LED_PIN, HIGH);
+    } else {
+      digitalWrite(RED_LED_PIN, LOW);
     }
+
+    delay(50);
   }
+
+  digitalWrite(GREEN_LED_PIN, HIGH);
 }
 
-int getTargetTemperature() {
-  int result = loadState(EEPROM_TARGET_TEMP);
+void loadTargetTemperature() {
+  _targetTemperature = loadState(EEPROM_TARGET_TEMP);
 
-  if (targetTemperature < 30 || targetTemperature > 200) {
-    result = 70;
+  if (_targetTemperature < 30 || _targetTemperature > 200) {
+    _targetTemperature = 70;
   }
-  
-  return result;
+
+#ifdef MY_DEBUG
+  Serial.print(F("Target Temperature in EEPROM: "));
+  Serial.println(_targetTemperature);
+#endif
+}
+
+void loadMode() {
+  _mode = loadState(EEPROM_MODE);
+
+  if (_mode < 1 || _mode > 2) {
+    _mode = 1;
+  }
+
+#ifdef MY_DEBUG
+  Serial.print(F("Mode in EEPROM: "));
+  Serial.println(_mode);
+#endif
+}
+
+void changeMode(int mode) {
+  if (mode == _mode) {
+    return;
+  }
+
+  saveState(EEPROM_MODE, mode);
+  _mode = mode;
+
+#ifdef MY_DEBUG
+  Serial.print(F("New mode: "));
+  Serial.println(_mode);
+#endif
+
+  pinMode(BLUE_LED_PIN, OUTPUT);
+
+  for (char i = 0; i < _mode * 2; i++) {
+    if (i % 2 == 0) {
+      digitalWrite(BLUE_LED_PIN, HIGH);
+    } else {
+      digitalWrite(BLUE_LED_PIN, LOW);
+    }
+
+    delay(1000);
+  }
+
+  digitalWrite(BLUE_LED_PIN, LOW);
+  pinMode(BLUE_LED_PIN, INPUT);
 }
 
 float getTemperature() {
@@ -244,20 +292,20 @@ float getTemperature() {
 }
 
 void reportBatteryLevel() {
-  const uint8_t batteryPcnt = static_cast<uint8_t>(0.5 + vcc.Read_Perc(VccMin, VccMax));
+  const uint8_t batteryPcnt = static_cast<uint8_t>(0.5 + _vcc.Read_Perc(_vccMin, _vccMax));
 
 #ifdef MY_DEBUG
   Serial.print(F("Vbat "));
-  Serial.print(vcc.Read_Volts());
+  Serial.print(_vcc.Read_Volts());
   Serial.print(F("\tPerc "));
   Serial.println(batteryPcnt);
 #endif
 
   // Battery readout should only go down. So report only when new value is smaller than previous one.
-  if ( batteryPcnt < oldBatteryPcnt )
+  if ( batteryPcnt < _oldBatteryPcnt )
   {
     sendBatteryLevel(batteryPcnt);
-    oldBatteryPcnt = batteryPcnt;
+    _oldBatteryPcnt = batteryPcnt;
   }
 }
 
