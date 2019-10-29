46829f9a7f211d869bd01499579c9f641bfd5d1c
Snyfir <Snyfir@users.noreply.github.com>
2019-10-29 21:46:53 +0100

    update CatRepellent

--- CatRepellent/CatRepellent.ino
+++ CatRepellent/CatRepellent.ino
@@ -10,39 +10,66 @@
 #define SENSOR_ID 0
 #define PUMP_PIN 5
 #define PIR_PIN 3
-#define LED_PIN A1
+#define GREEN_LED_PIN A1
+#define RED_LED_PIN A3
 #define WATER_SENSOR_PIN A0
 
+#define EEPROM_WATER_STATUS 0
+#define EEPROM_MODE 1
+#define EEPROM_DURATION 2
+#define EEPROM_SPACING 3
+
 MyMessage msg(SENSOR_ID, V_CUSTOM);
-Parser parser = Parser(' ');
-bool _waterTankEmpty = false;
-int _duration = 5;
-int _spacing = 1;
+Parser parser = Parser('-');
+bool _risingEvent;
+bool _waterTankEmpty;
+uint8_t _mode;
+uint8_t _duration;
+uint8_t _spacing;
 
 void before()
 {
   pinMode(PUMP_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+
   pinMode(PIR_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
   pinMode(WATER_SENSOR_PIN, INPUT);
+
   digitalWrite(PUMP_PIN, LOW);
-  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(GREEN_LED_PIN, LOW);
+  digitalWrite(RED_LED_PIN, HIGH);
 }
 
 void setup()
 {
+  _mode = readEeprom(EEPROM_MODE, 1);
+  _duration = readEeprom(EEPROM_DURATION, 2);
+  _spacing = readEeprom(EEPROM_SPACING, 0);
+  _waterTankEmpty = loadState(EEPROM_WATER_STATUS);
+
+#ifdef MY_DEBUG
+  Serial.println ("Mode: " + String(_mode));
+  Serial.println ("Duration: " + String(_duration));
+  Serial.println ("Spacing: " + String(_spacing));
+#endif
+
   request(0, V_CUSTOM, 0);
-  
-  for (byte i=0; i<20; i++) {
-    if (i%2 == 0) {
-      digitalWrite(LED_PIN, LOW);
+
+  digitalWrite(RED_LED_PIN, LOW);
+  for (byte i = 0; i < 20; i++) {
+    if (i % 2 == 0) {
+      digitalWrite(GREEN_LED_PIN, HIGH);
     } else {
-      digitalWrite(LED_PIN, HIGH);
+      digitalWrite(GREEN_LED_PIN, LOW);
     }
     wait(100);
   }
-  
+
   send(msg.set(F("cat repellent started")));
+
+  sleep(digitalPinToInterrupt(PIR_PIN), RISING, 0);
+  _risingEvent = true;
 }
 
 void presentation()
@@ -52,47 +79,119 @@ void presentation()
 }
 
 void receive(const MyMessage &myMsg)
-{ 
+{
   if (myMsg.type == V_CUSTOM && myMsg.sensor == 0) {
+#ifdef MY_DEBUG
     Serial.print("New message: ");
     Serial.println(myMsg.getString());
-    parser.parse(myMsg.getString());
+#endif
 
-    if (parser.get(0) != NULL && parser.get(1) != NULL) {
-      _duration = parser.getInt(0);
-      _spacing = parser.getInt(1);
-      Serial.println ("Duration: " + _duration);
-      Serial.println ("Spacing: " + _spacing);
-      send(msg.set(F("configuration updated")));
+    parser.parse(myMsg.getString());
+    if (parser.get(0) != NULL && parser.get(1) != NULL && parser.get(2) != NULL) {
+      _mode = parser.getInt(0);
+      _duration = parser.getInt(1);
+      _spacing = parser.getInt(2);
+
+#ifdef MY_DEBUG
+      Serial.println ("Mode: " + String(_mode));
+      Serial.println ("Duration: " + String(_duration));
+      Serial.println ("Spacing: " + String(_spacing));
+#endif
+
+      // Store mode in eeprom
+      if (loadState(EEPROM_MODE) != _mode) {
+        saveState(EEPROM_MODE, _mode);
+        send(msg.set(F("mode updated")));
+      }
+
+      // Store duration in eeprom
+      if (loadState(EEPROM_DURATION) != _duration) {
+        saveState(EEPROM_DURATION, _duration);
+        send(msg.set(F("duration updated")));
+      }
+
+      // Store spacing in eeprom
+      if (loadState(EEPROM_SPACING) != _spacing) {
+        saveState(EEPROM_SPACING, _spacing);
+        send(msg.set(F("spacing updated")));
+      }
     }
   }
 }
 
-
 void loop()
 {
-  if (digitalRead(PIR_PIN) == HIGH) {
-    if (!isWaterTankEmpty()) {
-      digitalWrite(PUMP_PIN, HIGH);
-      digitalWrite(LED_PIN, HIGH);
-      send(msg.set(F("cat alert")));
+  if (_mode == 3) {
+    if (digitalRead(PIR_PIN) == HIGH) {
+      digitalWrite(GREEN_LED_PIN, HIGH);
+    } else {
+      digitalWrite(GREEN_LED_PIN, LOW);
+    }
+  } else {
+    if (digitalRead(PIR_PIN) == HIGH) {
+      bool empty = isWaterTankEmpty();
+
+      if (empty) {
+        digitalWrite(RED_LED_PIN, HIGH);
+      } else {
+        digitalWrite(GREEN_LED_PIN, HIGH);
+      }
+
+      if ((!empty && _mode == 1) || _mode == 2) {
+        digitalWrite(PUMP_PIN, HIGH);
+      }
+
+      if (_risingEvent) {
+        send(msg.set(F("cat alert")));
+      }
+
+      // send water tank alert
+      if (!_waterTankEmpty && empty) {
+        send(msg.set(F("water tank is empty")));
+        _waterTankEmpty = true;
+        saveState(EEPROM_WATER_STATUS, _waterTankEmpty);
+      } else if (_waterTankEmpty && !empty) {
+        send(msg.set(F("water tank is full")));
+        _waterTankEmpty = false;
+        saveState(EEPROM_WATER_STATUS, _waterTankEmpty);
+      }
+
+      if (_risingEvent) {
+        request(0, V_CUSTOM, 0);
+      }
+
       wait(_duration * 1000);
+
       digitalWrite(PUMP_PIN, LOW);
-      digitalWrite(LED_PIN, LOW);
-      wait(_spacing * 1000);
-    } else if (!_waterTankEmpty) {
-      send(msg.set(F("water tank is empty")));
-      _waterTankEmpty = true;
+      digitalWrite(GREEN_LED_PIN, LOW);
+      digitalWrite(RED_LED_PIN, LOW);
+
+      if (_spacing > 0) {
+        sleep(_spacing * 1000);
+      }
+
+      _risingEvent = false;
+    } else {
+      sleep(digitalPinToInterrupt(PIR_PIN), RISING, 0);
+      _risingEvent = true;
     }
   }
-
-  sleep(digitalPinToInterrupt(PIR_PIN), RISING, 0);
 }
 
 bool isWaterTankEmpty() {
   pinMode(WATER_SENSOR_PIN, INPUT_PULLUP);
-  sleep(15);
+  wait(15);
   bool result = digitalRead(WATER_SENSOR_PIN) == HIGH;
   pinMode(WATER_SENSOR_PIN, INPUT);
   return result;
+}
+
+uint8_t readEeprom(uint8_t pos, uint8_t defaultValue) {
+  uint8_t value = loadState(pos);
+
+  if (value == 0xFF) {
+    return defaultValue;
+  } else {
+    return value;
+  }
 }
