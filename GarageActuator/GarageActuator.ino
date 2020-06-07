db2452129a13980117d7f678a28e075c15e94890
Snyfir <Snyfir@users.noreply.github.com>
2020-06-07 19:24:45 +0200

    GarageActuator

--- GarageActuator/GarageActuator.ino
+++ GarageActuator/GarageActuator.ino
@@ -16,21 +16,21 @@
 
 #define SERVO1_PIN 3
 #define SERVO2_PIN 5
-#define SERVO1_MIN 0
-#define SERVO1_MAX 180
-#define SERVO2_MIN 0
-#define SERVO2_MAX 180
+#define SERVO1_UNLOCK_POS 0
+#define SERVO1_LOCK_POS 180
+#define SERVO2_UNLOCK_POS 0
+#define SERVO2_LOCK_POS 180
 
-MyMessage msg(0, V_STATUS);
+MyMessage msg(1, V_CUSTOM);
 
 Servo _servo1;
 Servo _servo2;
 unsigned long _timeOfLastChange = 0;
-bool _servo1_action = false;
+bool _action = false;
 byte _servo1_position;
 byte _servo1_target;
-
-unsigned long _heartbeatTime = 0;
+byte _servo2_position;
+byte _servo2_target;
 
 void before()
 {
@@ -39,11 +39,18 @@ void before()
   _servo1.write(_servo1_position);
   delay(100);
   _servo1.detach();
-  _servo1_action = false;
+
+  _servo2_position = loadState(1); // Set position to last known state (using eeprom storage)
+  _servo2.attach(SERVO1_PIN);
+  _servo2.write(_servo2_position);
+  delay(100);
+  _servo2.detach();
+
+  _action = false;
 }
 
 void setup() {
- 
+  randomSeed(analogRead(0)); // A0
 }
 
 void presentation() {
@@ -52,52 +59,93 @@ void presentation() {
 
   // Present sensor to controller
   present(0, S_BINARY);
+  present(1, S_CUSTOM);
 }
 
 void receive(const MyMessage &message)
 {
   if (message.type == V_STATUS && message.sensor == 0) {
-    _servo1_action = true;
-    _timeOfLastChange = millis();
-  
     if (message.getBool()) {
-      Serial.println("lock");
-      _servo1_target = SERVO1_MAX;
+      Serial.println("lock cmd received");
+      _servo1_target = SERVO1_LOCK_POS;
+      _servo2_target = SERVO2_LOCK_POS;
     } else {
-      Serial.println("unlock");
-      _servo1_target = SERVO1_MIN;
+      Serial.println("unlock cmd received");
+      _servo1_target = SERVO1_UNLOCK_POS;
+      _servo2_target = SERVO2_UNLOCK_POS;
     }
 
-    // save in eeprom
-    if (loadState(0) != _servo1_target) {
-      Serial.println("save in epprom");
-      saveState(0, _servo1_target);
-    }
+    _action = true;
+    _timeOfLastChange = millis();
   }
 }
 
 inline void manageServo() {
-  if (_servo1_action && millis() - _timeOfLastChange > 5) {
-    if(!_servo1.attached()) {
-      Serial.println("attach servo 1");
+  if (_action && millis() - _timeOfLastChange > 5) {
+    // start servo 1
+    if (_servo1_target != _servo1_position && !_servo1.attached() && !_servo2.attached()) {
+      send(msg.set(F("moving servo 1...")));
       _servo1.attach(SERVO1_PIN);
     }
-    
-    if (_servo1_target > _servo1_position) {
-      _servo1_position++;
-      _servo1.write(_servo1_position);
-    } else if (_servo1_target < _servo1_position) {
-      _servo1_position--;
-      _servo1.write(_servo1_position);
-    } else {
-      if (_servo1.read() == _servo1_target) {
-        Serial.println("servo 1 position is ok");
+
+    // start servo 2
+    if (_servo2_target != _servo2_position && !_servo1.attached() && !_servo2.attached()) {
+      send(msg.set(F("moving servo 2...")));
+      _servo2.attach(SERVO2_PIN);
+    }
+
+    // Move servo 1
+    if (_servo1.attached()) {
+      if (_servo1_target > _servo1_position) {
+        _servo1_position++;
+        _servo1.write(_servo1_position);
+      } else if (_servo1_target < _servo1_position) {
+        _servo1_position--;
+        _servo1.write(_servo1_position);
       } else {
-        Serial.println("servo 1 position is KO");
+        if (_servo1_target == SERVO1_UNLOCK_POS) {
+          send(msg.set(F("servo 1 unlocked")));
+        } else {
+          send(msg.set(F("servo 1 locked")));
+        }
+
+        // save in eeprom
+        if (loadState(0) != _servo1_target) {
+          saveState(0, _servo1_target);
+        }
+
+        _servo1.detach();
       }
-      
-      _servo1.detach();
-      _servo1_action = false;
+      // Move servo 2
+    } else if (_servo2.attached()) {
+      if (_servo2_target > _servo2_position) {
+        _servo2_position++;
+        _servo2.write(_servo2_position);
+      } else if (_servo2_target < _servo2_position) {
+        _servo2_position--;
+        _servo2.write(_servo2_position);
+      } else {
+        if (_servo2_target == SERVO2_UNLOCK_POS) {
+          send(msg.set(F("servo 2 unlocked")));
+        } else {
+          send(msg.set(F("servo 2 locked")));
+        }
+
+        // save in eeprom
+        if (loadState(1) != _servo2_target) {
+          saveState(1, _servo2_target);
+        }
+
+        _servo2.detach();
+      }
+    } else if (_servo1_target == _servo1_position && _servo2_target == _servo2_position) {
+      _action = false;
+
+      if (_servo1_target == SERVO1_UNLOCK_POS) {
+        send(msg.set(F("Unlocked")));
+      } else {
+        send(msg.set(F("Locked")));
+      }
     }
 
     _timeOfLastChange = millis();
@@ -106,9 +154,30 @@ inline void manageServo() {
 
 void loop() {
   manageServo();
-  
-  if (millis() - _heartbeatTime >= 60000) {
-    sendHeartbeat();
-    _heartbeatTime = millis();
+  manageHeartbeat();
+}
+
+inline void manageHeartbeat() {
+  static unsigned long _heartbeatLastSend = 0;
+  static unsigned long _heartbeatWait = random(1000, 60000);
+  static unsigned long _heartbeatRetryNb = 0;
+
+  if (millis() - _heartbeatLastSend >= _heartbeatWait) {
+    bool success = sendHeartbeat();
+
+    if (success) {
+      _heartbeatWait = 60000;
+      _heartbeatRetryNb = 0;
+    } else {
+      if (_heartbeatRetryNb < 10) {
+        _heartbeatWait = random(100, 3000);
+        _heartbeatRetryNb++;
+      } else {
+        _heartbeatWait = random(45000, 60000);
+        _heartbeatRetryNb = 0;
+      }
+    }
+    
+    _heartbeatLastSend = millis();
   }
 }
