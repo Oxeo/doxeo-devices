e565404be6261f271790e8c44297593b44404daf
Snyfir <Snyfir@users.noreply.github.com>
2021-02-27 22:39:21 +0100

    update CatRepellent

--- CatRepellent/CatRepellent.ino
+++ CatRepellent/CatRepellent.ino
@@ -1,8 +1,6 @@
 //#define MY_DEBUG
 
 #define MY_RADIO_RF24
-//#define MY_PARENT_NODE_ID 19
-//#define MY_PARENT_NODE_IS_STATIC
 
 // Enable IRQ pin
 #define MY_RX_MESSAGE_BUFFER_FEATURE
@@ -27,20 +25,19 @@ uint8_t _state;
 MyMessage msg(0, V_CUSTOM);
 Parser parser = Parser(' ');
 unsigned long _cpt = 0;
-bool _waterTankEmpty;
-unsigned long _duration = 10;
-unsigned long _startTime = 0;
-float _voltageCorrection = 1;
-int _batteryPercent = 0;
+unsigned long _runningTime = 0;
+unsigned long _stateTimer = 0;
+float _voltageCorrection = 1.0;
+int _batteryPercent = 101;
 
 void before()
 {
   pinMode(PUMP_PIN, INPUT);
   pinMode(WATER_SENSOR_PIN, INPUT);
   pinMode(BATTERY_LEVEL_PIN, INPUT);
-  _waterTankEmpty = false;
   _state = SLEEPING;
   _cpt = 0;
+  _batteryPercent = 101;
 
   //saveVoltageCorrection(0.9845); // Measured by multimeter divided by reported
   _voltageCorrection = getVoltageCorrection();
@@ -51,7 +48,7 @@ void before()
 
 void setup()
 {
-  send(msg.set(F("cat repellent started")));
+  reportBatteryLevel();
 }
 
 void presentation()
@@ -66,9 +63,15 @@ void receive(const MyMessage &message)
     parser.parse(message.getString());
 
     if (parser.isEqual(0, "stop")) {
-      stopPump();
+      changeState(SLEEPING);
     } else if (parser.isEqual(0, "start") && parser.get(1) != NULL) {
-      startPump(parser.getInt(1));
+      _runningTime = parser.getInt(1);
+
+      if (_state != RUNNING) {
+        changeState(RUNNING);
+      } else {
+        _stateTimer = millis();
+      }
     } else {
       send(msg.set(F("invalid command")));
     }
@@ -81,28 +84,21 @@ void loop()
     sleep(985);
     RF24_startListening();
     wait(15);
-
     _cpt += 1;
+    
     if (_cpt == 21600) { // 6H
       _cpt = 0;
       reportBatteryLevel();
       sendHeartbeat();
     }
   } else if (_state == RUNNING) {
-    managePump();
-  }
-}
+    if (millis() - _stateTimer > _runningTime * 1000UL) {
+      changeState(SLEEPING);
+    }
 
-inline void managePump() {
-  if (millis() - _startTime > _duration) {
-    stopPump();
-  }
-
-  if (millis() - _startTime > 100UL) {
-    if (digitalRead(WATER_SENSOR_PIN) == HIGH) {
-      stopPump();
-      _waterTankEmpty = true;
+    if (isWaterTankEmpty()) {
       send(msg.set(F("water tank is empty")));
+      changeState(SLEEPING);
     }
   }
 }
@@ -110,42 +106,33 @@ inline void managePump() {
 void changeState(uint8_t state) {
   switch (state) {
     case SLEEPING:
+      pinMode(WATER_SENSOR_PIN, INPUT);
+      stopPump();
+      send(msg.set(F("stopped")));
       break;
     case RUNNING:
-      _startTime = millis();
+      pinMode(WATER_SENSOR_PIN, INPUT_PULLUP);
+      wait(15);
+      send(msg.set(F("started")));
+      startPump();
       break;
   }
 
   _state = state;
+  _stateTimer = millis();
 }
 
-void startPump(int duration) {
-  if (_waterTankEmpty && isWaterTankEmpty()) {
-    send(msg.set(F("water tank is empty")));
-    return;
-  }
-  
-  send(msg.set(F("started")));
-  _duration = duration * 1000UL;
+void startPump() {
   pinMode(PUMP_PIN, OUTPUT);
   digitalWrite(PUMP_PIN, LOW);
-  pinMode(WATER_SENSOR_PIN, INPUT_PULLUP);
-  changeState(RUNNING);
 }
 
 void stopPump() {
   pinMode(PUMP_PIN, INPUT);
-  pinMode(WATER_SENSOR_PIN, INPUT);
-  send(msg.set(F("stopped")));
-  changeState(SLEEPING);
 }
 
 bool isWaterTankEmpty() {
-  pinMode(WATER_SENSOR_PIN, INPUT_PULLUP);
-  wait(15);
-  bool result = digitalRead(WATER_SENSOR_PIN) == HIGH;
-  pinMode(WATER_SENSOR_PIN, INPUT);
-  return result;
+  return digitalRead(WATER_SENSOR_PIN) == HIGH;
 }
 
 void reportBatteryLevel() {
@@ -159,7 +146,7 @@ void reportBatteryLevel() {
   Serial.println(F("%)"));
 #endif
 
-  if (gauge.percent != _batteryPercent) {
+  if (gauge.percent < _batteryPercent) {
     String voltageMsg = "voltage-" + String(gauge.voltage) + "-" + String(gauge.percent);
     send(msg.set(voltageMsg.c_str()));
     sendBatteryLevel(gauge.percent);
