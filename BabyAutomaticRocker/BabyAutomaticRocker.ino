0184b35d1f56734d7ebb17b882ebc4f59fdc03a0
Snyfir <Snyfir@users.noreply.github.com>
2022-07-17 18:29:47 +0200

    add TMC2208Stepper library

--- BabyAutomaticRocker/BabyAutomaticRocker.ino
+++ BabyAutomaticRocker/BabyAutomaticRocker.ino
@@ -1,17 +1,19 @@
+#include <TMC2208Stepper.h>
+
 #define MICROSTEP 200 * 2
  
 #define MOTOR1_DIR_PIN 8
 #define MOTOR1_STEP_PIN 4
 #define MOTOR1_SLP_PIN 9
-#define MOTOR1_ENA_PIN A2
+#define MOTOR1_UART_PIN A2
 
 #define MOTOR2_DIR_PIN 11
 #define MOTOR2_STEP_PIN 10
 #define MOTOR2_SLP_PIN 12
 
+TMC2208Stepper driver = TMC2208Stepper(A3, MOTOR1_UART_PIN);
+
 unsigned long previousTime = micros();
-
-
 unsigned long stepTimer = millis();
 byte stepNb = 0;
 byte direction = HIGH;
@@ -25,9 +27,11 @@ float currentRpm;
  
 void setup()
 {  
-    //pinMode(MOTOR1_SLP_PIN, OUTPUT);
-    pinMode(MOTOR1_SLP_PIN, INPUT);
-    pinMode(MOTOR1_ENA_PIN, OUTPUT);
+    driver.beginSerial(115200);
+    // Push at the start of setting up the driver resets the register to default
+    driver.push();
+  
+    pinMode(MOTOR1_SLP_PIN, OUTPUT);
     pinMode(MOTOR2_SLP_PIN, OUTPUT);
 
     pinMode(MOTOR1_DIR_PIN, OUTPUT);
@@ -38,10 +42,21 @@ void setup()
 
     Serial.begin(115200);
 
+    driver.pdn_disable(true);     // Use PDN/UART pin for communication
+    driver.I_scale_analog(false); // Use internal voltage reference
+    driver.rms_current(700);      // Set driver current = 500mA, 0.5 multiplier for hold current and RSENSE = 0.11.
+    driver.mstep_reg_select(1);   // Microstep resolution selected by MSTEP register
+    driver.microsteps(2);         // Set number of microsteps
+    driver.TPWMTHRS(50);          // When the velocity exceeds the limit set by TPWMTHRS, the driver switches to spreadCycle
+    //driver.en_spreadCycle(false);
+    
+    //driver.VACTUAL(1000);
+    driver.toff(2);               // Enable driver in software*/
+
     wakeUpMotors();
 
     digitalWrite(MOTOR1_DIR_PIN,HIGH);
-    //digitalWrite(MOTOR2_DIR_PIN,LOW);
+    digitalWrite(MOTOR2_DIR_PIN,HIGH);
 
     stepNb = 0;
     stepTimer = millis();
@@ -95,6 +110,7 @@ inline void manageAcceleration() {
         if (currentRpm < 5.0) {
           direction = direction == HIGH ? LOW : HIGH;
           digitalWrite(MOTOR1_DIR_PIN, direction);
+          digitalWrite(MOTOR2_DIR_PIN, direction);
           stepNb = 0;
         }
         
@@ -128,13 +144,12 @@ void stealthChop2Autotune() {
 }
 
 void sleepMotors() {
-  //digitalWrite(MOTOR1_SLP_PIN, LOW);
+  digitalWrite(MOTOR1_SLP_PIN, HIGH);
   digitalWrite(MOTOR2_SLP_PIN, LOW);
 }
 
 void wakeUpMotors() {
-  //digitalWrite(MOTOR1_SLP_PIN, HIGH);
-  digitalWrite(MOTOR1_ENA_PIN, LOW);
+  digitalWrite(MOTOR1_SLP_PIN, LOW);
   digitalWrite(MOTOR2_SLP_PIN, HIGH);
   delay(1);
 }
