fb24c6b6af9489bf31edee14525e4d01be15fe80
Snyfir <Snyfir@users.noreply.github.com>
2025-04-12 23:11:23 +0200

    update FingerPrintAlarm

--- FingerPrintAlarm/FingerPrintAlarm.ino
+++ FingerPrintAlarm/FingerPrintAlarm.ino
@@ -10,16 +10,20 @@
 #define MY_SIGNING_ATSHA204
 #define MY_SIGNING_REQUEST_SIGNATURES
 
+#include <Adafruit_Fingerprint.h>
 #include <MySensors.h>
 #include <Parser.h>
-#include <Adafruit_Fingerprint.h>
 
 #define FINGER_RX 4
 #define FINGER_TX 5
 #define FINGER_WAKEUP 3
 #define FINGER_POWER 6
 
-MyMessage _customMsg(0, V_CUSTOM);
+#define CHILD_ID_INFO_MSG 0
+#define CHILD_ID_RESPONSE_MSG 1
+
+MyMessage _msgInfo(CHILD_ID_INFO_MSG, V_CUSTOM);
+MyMessage _msgResponse(CHILD_ID_RESPONSE_MSG, V_TEXT);
 Parser _parser = Parser(' ');
 SoftwareSerial _fingerSerial(FINGER_TX, FINGER_RX);
 Adafruit_Fingerprint _finger = Adafruit_Fingerprint(&_fingerSerial);
@@ -27,10 +31,15 @@ unsigned long _timer = 0;
 bool _successMsgReceived = false;
 bool _configMode = false;
 
-void before()
-{
+void before() {
   pinMode(FINGER_WAKEUP, INPUT);
   pinMode(FINGER_POWER, INPUT);
+}
+
+void presentation() {
+  sendSketchInfo("Fingerprint Alarm", "1.1");
+  present(CHILD_ID_INFO_MSG, S_CUSTOM, "Fingerprint");
+  present(CHILD_ID_RESPONSE_MSG, S_INFO, "Response message");
 }
 
 void setup() {
@@ -40,23 +49,19 @@ void setup() {
   delay(5);
 
   if (_finger.verifyPassword()) {
-    send(_customMsg.set(F("fingerprint ok")));
+    send(_msgInfo.set(F("fingerprint ok")));
     _finger.getTemplateCount();
     String templates = "Templates: " + String(_finger.templateCount);
-    send(_customMsg.set(templates.c_str()));
+    send(_msgInfo.set(templates.c_str()));
   } else {
-    send(_customMsg.set(F("fingerprint not found")));
+    send(_msgInfo.set(F("fingerprint not found")));
   }
+
+  send(_msgResponse.set("started"));
 }
 
-void presentation() {
-  sendSketchInfo("Fingerprint Alarm", "1.0");
-  present(0, S_CUSTOM, "Fingerprint");
-}
-
-void receive(const MyMessage &message)
-{
-  if (message.type == V_CUSTOM && message.sensor == 0 && !message.isAck()) {
+void receive(const MyMessage &message) {
+  if (message.sensor == CHILD_ID_INFO_MSG && message.type == V_CUSTOM && !message.isAck()) {
     _parser.parse(message.getString());
 
     if (_parser.isEqual(0, "success")) {
@@ -78,28 +83,34 @@ void receive(const MyMessage &message)
       startFingerPrint();
       _finger.getTemplateCount();
       String templates = "Templates: " + String(_finger.templateCount);
-      send(_customMsg.set(templates.c_str()));
+      send(_msgInfo.set(templates.c_str()));
       stopFingerPrint();
     } else if (_parser.isEqual(0, "delete-all")) {
       startFingerPrint();
       _finger.emptyDatabase();
       enrollFingerprintWithMessage(1);
-      send(_customMsg.set(F("database erased")));
+      send(_msgInfo.set(F("database erased")));
       stopFingerPrint();
     } else if (_parser.isEqual(0, "start-config")) {
       _configMode = true;
       _timer = millis();
-      send(_customMsg.set(F("start config mode")));
+      send(_msgInfo.set(F("start config mode")));
     } else if (_parser.isEqual(0, "stop-config")) {
       _configMode = false;
-      send(_customMsg.set(F("stop config mode")));
+      send(_msgInfo.set(F("stop config mode")));
+    }
+  } else if (message.sensor == CHILD_ID_RESPONSE_MSG && message.type == V_TEXT && !message.isAck()) {
+    const char *receivedMessage = message.getString();
+
+    if (strcmp(receivedMessage, "success") == 0) {
+      _successMsgReceived = true;
     }
   }
 }
 
 void loop() {
   if (_configMode) {
-    if (millis() - _timer > 300000UL) { // 5 minutes
+    if (millis() - _timer > 300000UL) {  // 5 minutes
       _configMode = false;
     } else {
       wait(100);
@@ -136,14 +147,15 @@ void loop() {
 void foundMatch() {
   _finger.LEDcontrol(FINGERPRINT_LED_ON, 0, FINGERPRINT_LED_BLUE);
   _successMsgReceived = false;
-  String message = "match-" + String(_finger.fingerID);
+  // String message = "match-" + String(_finger.fingerID);
+  String message = "matched";
 
-  if (sendWithRetry(_customMsg.set(message.c_str()), 10)) {
-    wait(500, _customMsg.getCommand(), _customMsg.type);
+  if (sendWithRetry(_msgInfo.set(message.c_str()), 10)) {
+    wait(500, C_SET, V_TEXT);
 
     if (_successMsgReceived) {
       delay(300);
-      send(_customMsg.set(F("released")));
+      send(_msgInfo.set(F("released")));
       delay(500);
 
       _finger.LEDcontrol(FINGERPRINT_LED_FLASHING, 25, FINGERPRINT_LED_BLUE, 10);
@@ -153,7 +165,7 @@ void foundMatch() {
   }
 
   delay(300);
-  send(_customMsg.set(F("released")));
+  send(_msgInfo.set(F("released")));
   delay(500);
 
   _finger.LEDcontrol(FINGERPRINT_LED_FLASHING, 25, FINGERPRINT_LED_RED, 10);
@@ -162,9 +174,9 @@ void foundMatch() {
 
 void noMatch() {
   _finger.LEDcontrol(FINGERPRINT_LED_FLASHING, 25, FINGERPRINT_LED_RED, 10);
-  send(_customMsg.set(F("no match")));
+  send(_msgInfo.set(F("no match")));
   delay(300);
-  send(_customMsg.set(F("released")));
+  send(_msgInfo.set(F("released")));
   wait(2000);
 }
 
@@ -189,7 +201,7 @@ void startFingerPrint() {
   pinMode(FINGER_POWER, OUTPUT);
   digitalWrite(FINGER_POWER, LOW);
   pinMode(FINGER_RX, OUTPUT);
-  _fingerSerial.println("a"); // fix first send
+  _fingerSerial.println("a");  // fix first send
   delay(30);
 }
 
@@ -206,6 +218,7 @@ boolean sendWithRetry(MyMessage &message, const byte retryNumber) {
     success = send(message, true);
 
     if (success) {
+      // wait echo message of the destination node
       success = wait(500, message.getCommand(), message.type);
     }
 
@@ -219,18 +232,18 @@ boolean sendWithRetry(MyMessage &message, const byte retryNumber) {
 
 boolean enrollFingerprintWithMessage(uint8_t id) {
   if (id < 1 || id > 127) {
-    send(_customMsg.set(F("Wrong id")));
+    send(_msgInfo.set(F("Wrong id")));
     return false;
   }
 
   String templates = "Start enrolling " + String(id);
-  send(_customMsg.set(templates.c_str()));
+  send(_msgInfo.set(templates.c_str()));
 
   uint8_t result = enrollFingerprint(id);
 
   if (result == FINGERPRINT_OK) {
     String templates = String(id) + F(" stored with success!");
-    send(_customMsg.set(templates.c_str()));
+    send(_msgInfo.set(templates.c_str()));
     delay(2000);
     return true;
   } else {
@@ -307,34 +320,34 @@ boolean enrollFingerprint(uint8_t id) {
 void sendFingerError(uint8_t error) {
   switch (error) {
     case FINGERPRINT_IMAGEMESS:
-      send(_customMsg.set(F("Image too messy")));
+      send(_msgInfo.set(F("Image too messy")));
       break;
     case FINGERPRINT_FEATUREFAIL:
-      send(_customMsg.set(F("Features fail")));
+      send(_msgInfo.set(F("Features fail")));
       break;
     case FINGERPRINT_INVALIDIMAGE:
-      send(_customMsg.set(F("Invalid image")));
+      send(_msgInfo.set(F("Invalid image")));
       break;
     case FINGERPRINT_PACKETRECIEVEERR:
-      send(_customMsg.set(F("Communication error")));
+      send(_msgInfo.set(F("Communication error")));
       break;
     case FINGERPRINT_BADLOCATION:
-      send(_customMsg.set(F("Bad location")));
+      send(_msgInfo.set(F("Bad location")));
       break;
     case FINGERPRINT_IMAGEFAIL:
-      send(_customMsg.set(F("Imaging error")));
+      send(_msgInfo.set(F("Imaging error")));
       break;
     case FINGERPRINT_FLASHERR:
-      send(_customMsg.set(F("Error writing to flash")));
+      send(_msgInfo.set(F("Error writing to flash")));
       break;
     case FINGERPRINT_ENROLLMISMATCH:
-      send(_customMsg.set(F("Finger did not match")));
+      send(_msgInfo.set(F("Finger did not match")));
       break;
     case FINGERPRINT_NOFINGER:
-      send(_customMsg.set(F("No finger")));
+      send(_msgInfo.set(F("No finger")));
       break;
     default:
-      send(_customMsg.set(F("Unknown error")));
+      send(_msgInfo.set(F("Unknown error")));
       break;
   }
 }
